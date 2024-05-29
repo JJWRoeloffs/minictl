31edd4fde355697c2c075133146c8800a30e29b3
JJWRoeloffs <jelleroeloffs@gmail.com>
2024-05-29 23:37:57 +0200

    Hide pyo3 bindings behind python comp feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,13 @@
 pub mod parser;
+
+#[cfg(feature = "python")]
+mod pythonutils;
+
+#[cfg(feature = "python")]
+use pyo3::prelude::*;
+
+#[cfg(feature = "python")]
+#[pymodule]
+fn minictl(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
+    Ok(())
+}
