5ca84da1fcd4de82bf5dd3695b3656ed5717f00c
JJWRoeloffs <jelleroeloffs@gmail.com>
2024-05-29 22:30:08 +0200

    Remove debug print statements

--- tests/test_parser.rs
+++ tests/test_parser.rs
@@ -7,7 +7,6 @@ mod common;
 #[test]
 fn all_ispl_files_are_valid() {
     for path in common::get_paths_by_ext(&common::get_testdata_dir(), "ispl").unwrap() {
-        dbg!(&path);
         let contents = fs::read_to_string(path).expect("Files exist");
         let tokens = parser::tokenize(&contents);
         assert!(tokens
