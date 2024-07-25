d39518fb8bfe53b99d6073b463d16f2bcc4d6109
JJWRoeloffs <jelleroeloffs@gmail.com>
2024-07-25 19:50:28 +0200

    Update tests to match new structure

--- tests/test_parser.rs
+++ tests/test_parser.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-use minictl::parser;
+use minictl::ispl;
 
 mod common;
 
@@ -8,7 +8,7 @@ mod common;
 fn all_ispl_files_are_valid() {
     for path in common::get_paths_by_ext(&common::get_testdata_dir(), "ispl").unwrap() {
         let contents = fs::read_to_string(path).expect("Files exist");
-        let tokens = parser::tokenize(&contents);
+        let tokens = ispl::tokenize(&contents);
         assert!(tokens.iter().all(|(token, _str)| !token.is_err()))
     }
 }
