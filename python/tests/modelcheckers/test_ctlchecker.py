a68f47ec18ed16a7205ce86a46b64bf39113d839
JJWRoeloffs <jelleroeloffs@gmail.com>
2025-03-09 12:57:23 +0100

    Add CTLChecker test from lecture slides

--- python/tests/modelcheckers/test_ctlchecker.py
+++ python/tests/modelcheckers/test_ctlchecker.py
@@ -167,3 +167,59 @@ class TestCheckerBasics:
         checker = CTLChecker(self.model)
         assert checker.check(CTLFormula.parse("EGp")) == {"s1", "s2"}
         assert checker.check(CTLFormula.parse("EGq")) == {"s2", "s3", "s5", "s6"}
+
+
+# These come from lecture and workgroup slides of the course.
+# I thought making it do all assignments would be a good and funny test. :P
+class TestCheckerFormulas:
+    s1 = State("s1", {"p"})
+    s2 = State("s2", {"p", "q"})
+    s3 = State("s3", {"p", "q"})
+    s4 = State("s4", set())
+    s5 = State("s5", {"q"})
+    s6 = State("s6", {"q"})
+    model = Model(
+        [s1, s2, s3, s4, s5, s6],
+        {
+            "s1": ["s1", "s2", "s3"],
+            "s2": ["s1", "s2", "s3"],
+            "s3": ["s4", "s5"],
+            "s4": ["s1", "s6"],
+            "s5": ["s4", "s6"],
+            "s6": ["s1", "s2"],
+        },
+    )
+
+    def test1(self):
+        checker = CTLChecker(self.model)
+        assert checker.check(CTLFormula.parse("E[!pU!q]")) == {"s1", "s4", "s5", "s6"}
+
+    def test2(self):
+        checker = CTLChecker(self.model)
+        assert checker.check(CTLFormula.parse("E[pU!q]")) == {"s1", "s2", "s3", "s4"}
+
+    def test3(self):
+        checker = CTLChecker(self.model)
+        assert checker.check(CTLFormula.parse("A[pU(EXq)]")) == {
+            "s1",
+            "s2",
+            "s3",
+            "s4",
+            "s5",
+            "s6",
+        }
+
+    def test4(self):
+        checker = CTLChecker(self.model)
+        assert checker.check(CTLFormula.parse("AX EXp")) == {"s4", "s5", "s6"}
+
+    def test5(self):
+        checker = CTLChecker(self.model)
+        assert checker.check(CTLFormula.parse("AF EGp")) == {
+            "s1",
+            "s2",
+            "s3",
+            "s4",
+            "s5",
+            "s6",
+        }
